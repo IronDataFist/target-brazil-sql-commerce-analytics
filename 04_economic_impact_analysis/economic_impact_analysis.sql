a8aff664c19769799cf95f515f58efb72dae633d
SIDDHESH DINESH NIVATKAR <vsid.nivatkar07@gmail.com>
2026-04-13 00:14:06 +0530

    Add cost increase analysis for orders between 2017 and 2018
    Added a query to calculate the percentage increase in order costs from 2017 to 2018 for the months of January to August.

--- 04_economic_impact_analysis/economic_impact_analysis.sql
+++ 04_economic_impact_analysis/economic_impact_analysis.sql
@@ -1,4 +1,5 @@
 ## Impact on Economy: Analyze the money movement by e-commerce by looking at order prices, freight and others.
+       
 # 1. Calculate the Total & Average value of order price for each state.
 # 2. Calculate the Total & Average value of order freight for each state.
 WITH order_level_metrix AS (
@@ -28,3 +29,29 @@ SELECT customer_state,
 FROM order_level_metrix
 GROUP BY customer_state
 ORDER BY total_order_value DESC;
+
+
+# 3. Get the % increase in the cost of orders from year 2017 to 2018 (include months between Jan to Aug only).
+    # You can use the "payment_value" column in the payments table to get the cost of orders.
+WITH cost_of_orders_table AS (
+SELECT p.order_id,
+       o.order_purchase_timestamp,
+       SUM(p.payment_value) AS order_value
+FROM `Target_Corporation.payments` p
+JOIN `Target_Corporation.orders` o
+ON p.order_id = o.order_id
+WHERE (EXTRACT(YEAR FROM o.order_purchase_timestamp) = 2017) OR
+      (EXTRACT(YEAR FROM o.order_purchase_timestamp) = 2018) AND
+      (EXTRACT(MONTH FROM o.order_purchase_timestamp) BETWEEN 1 AND 8)
+GROUP BY p.order_id, o.order_purchase_timestamp),
+  
+yearly_cost AS (
+SELECT EXTRACT(YEAR FROM order_purchase_timestamp) AS year,
+       ROUND(SUM(order_value), 2) AS total_order_value
+FROM cost_of_orders_table
+GROUP BY year)
+
+SELECT year,
+       total_order_value,
+       ROUND(SAFE_DIVIDE((total_order_value - LAG(total_order_value) OVER (ORDER BY year ASC)), LAG(total_order_value) OVER (ORDER BY year ASC)) * 100, 2) AS pct_increase
+FROM yearly_cost;
